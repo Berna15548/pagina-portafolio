abe746a8054657ae4c4f7c61f8e4bc180e76d6b0
Werner <wernertraveleraus@gmail.com>
2025-12-05 03:24:45 -0300

    loader y mas cosas

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,36 @@ import Inicio from "./components/Inicio.tsx";
 import Proyectos from "./components/Proyectos.tsx";
 import Contacto from "./components/Contacto.tsx";
 import ParticlesComponent from "./components/ParticlesComponent.tsx";
+import {useState} from "react";
+import {CssLoader} from "./components/CssLoader.tsx";
+import {useEffect} from "react";
 
 function App() {
 
+    const [loading, setLoading] = useState(true);
 
-
+    // Simular tiempo de carga
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 2000); // 2s de loader
+        return () => clearTimeout(timer);
+    }, []);
+    /*  ====== pantalla de carga   ========    */
+    if (loading) {
+        return (
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundColor: "black",
+                width: "100vw",
+            }}>
+                <CssLoader  />
+            </div>
+        );
+    }
+    
+    /*      ========        app         ========*/
     return (
         <>
             <div id="app-container">
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,173 +1,88 @@
-import {Badge} from "@mantine/core";
-import {SiReact} from "react-icons/si";
-import {SiAngular} from "react-icons/si";
-import {SiFigma} from "react-icons/si";
-import {SiJavascript} from "react-icons/si";
-import {SiTypescript} from "react-icons/si";
-import {SiCss3} from "react-icons/si";
-import {SiSass} from "react-icons/si";
-import {SiBootstrap} from "react-icons/si";
-import {SiMantine} from "react-icons/si";
-import {SiMaterialdesign} from "react-icons/si";
-import {SiMysql} from "react-icons/si";
-import {SiFirebase} from "react-icons/si";
-import {SiPostman} from "react-icons/si";
-import {SiSwagger} from "react-icons/si";
-import {SiIntellijidea} from "react-icons/si";
-import {VscVscodeInsiders} from "react-icons/vsc";
-import {SiTrello} from "react-icons/si";
-import {SiAdobephotoshop} from "react-icons/si";
-import {SiKrita} from "react-icons/si";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFileExcel} from "@fortawesome/free-solid-svg-icons";
-import {SiGoogledocs} from "react-icons/si";
+import SkillBadge from "./SkillBadge";
+
+import {
+    SiReact,
+    SiAngular,
+    SiFigma,
+    SiJavascript,
+    SiTypescript,
+    SiCss3,
+    SiSass,
+    SiBootstrap,
+    SiMantine,
+    SiMaterialdesign,
+    SiMysql,
+    SiFirebase,
+    SiPostman,
+    SiSwagger,
+    SiIntellijidea,
+    SiTrello,
+    SiAdobephotoshop,
+    SiKrita,
+    SiGoogledocs,
+} from "react-icons/si";
+
+import { VscVscodeInsiders } from "react-icons/vsc";
+import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Skills = () => {
-    
-    
-    
+    const front = [
+        { icon: SiReact, label: "React" },
+        { icon: SiAngular, label: "Angular" },
+        { icon: SiFigma, label: "Figma" },
+        { icon: SiJavascript, label: "JavaScript" },
+        { icon: SiTypescript, label: "Typescript" },
+        { icon: SiCss3, label: "Css" },
+        { icon: SiSass, label: "Scss" },
+        { icon: SiBootstrap, label: "Bootstrap" },
+        { icon: SiMantine, label: "Mantine" },
+        { icon: SiMaterialdesign, label: "Materials" },
+    ];
+
+    const backend = [
+        { icon: SiMysql, label: "MySQL" },
+        { icon: SiFirebase, label: "Firebase" },
+        { icon: SiPostman, label: "Postman" },
+        { icon: SiSwagger, label: "Swagger" },
+    ];
+
+    const aux = [
+        { icon: SiIntellijidea, label: "IntelliJ" },
+        { icon: VscVscodeInsiders, label: "VSC" },
+        { icon: SiTrello, label: "Trello" },
+        { icon: SiAdobephotoshop, label: "Photoshop" },
+        { icon: SiKrita, label: "Krita" },
+        { icon: (props: any) => <FontAwesomeIcon {...props} icon={faFileExcel} />, label: "Excel" },
+        { icon: SiGoogledocs, label: "Google Docs" },
+    ];
+
     return (
-        <div className="cuadro-sobremi" style={{height: "fit-content!important"}}>
+        <div className="cuadro-sobremi" style={{ height: "fit-content" }}>
             <div id="skills">
                 <h3>Herramientas de Front</h3>
                 <div className="skills-badge">
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiReact/>
-                        </div>
-                        React
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiAngular/>
-                        </div>
-                        Angular
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiFigma/>
-                        </div>
-                        Figma
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiJavascript/>
-                        </div>
-                        JavaScript
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiTypescript/>
-                        </div>
-                        Typescript
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiCss3/>
-                        </div>
-                        Css
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiSass/>
-                        </div>
-                        Scss
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiBootstrap/>
-                        </div>
-                        Bootstrap
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiMantine/>
-                        </div>
-                        Mantine
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiMaterialdesign/>
-                        </div>
-                        Materials
-                    </Badge>
+                    {front.map((item) => (
+                        <SkillBadge key={item.label} icon={item.icon} label={item.label} />
+                    ))}
                 </div>
 
                 <h3>Herramientas Backend</h3>
                 <div className="skills-badge">
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiMysql/>
-                        </div>
-                        MySQL
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiFirebase/>
-                        </div>
-                        Firebase
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiPostman/>
-                        </div>
-                        Postman
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiSwagger/>
-                        </div>
-                        Swagger
-                    </Badge>
+                    {backend.map((item) => (
+                        <SkillBadge key={item.label} icon={item.icon} label={item.label} />
+                    ))}
                 </div>
 
                 <h3>Herramientas Auxiliares</h3>
                 <div className="skills-badge">
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiIntellijidea/>
-                        </div>
-                        IntelliJ
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <VscVscodeInsiders/>
-                        </div>
-                        VSC
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiTrello/>
-                        </div>
-                        Trello
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiAdobephotoshop/>
-                        </div>
-                        Photoshop
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiKrita/>
-                        </div>
-                        Krita
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <FontAwesomeIcon icon={faFileExcel}/>
-                        </div>
-                        Excel
-                    </Badge>
-                    <Badge>
-                        <div className="skill-icon">
-                            <SiGoogledocs/>
-                        </div>
-                        Google Docs
-                    </Badge>
+                    {aux.map((item) => (
+                        <SkillBadge key={item.label} icon={item.icon} label={item.label} />
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
-export default Skills
+    );
+};
+
+export default Skills;
